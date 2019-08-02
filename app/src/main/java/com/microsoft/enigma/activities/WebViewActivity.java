d81857276d6ff594842a4adb1d57457eed5255ef
alphadose <anish.mukherjee1996@gmail.com>
2019-08-02 18:24:56 +0530

    Add User Model

--- app/src/main/java/com/microsoft/enigma/activities/WebViewActivity.java
+++ app/src/main/java/com/microsoft/enigma/activities/WebViewActivity.java
@@ -3,11 +3,14 @@ package com.microsoft.enigma.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.microsoft.enigma.R;
+import com.microsoft.enigma.models.UserModel;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,6 +22,7 @@ import java.util.Map;
 
 public class WebViewActivity extends AppCompatActivity {
 
+    private WebView webView;
     private final static String VOTER_SEARCH_URL = "https://electoralsearch.in/";
     private final static String VOTER_RESULT_URL = "https://electoralsearch.in/##resultArea";
     private final static String JAVASCRIPT_QUERY = "javascript:window.HTMLOUT.processHTML('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');";
@@ -26,9 +30,9 @@ public class WebViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().requestFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.web_activity);
-
-        final WebView webView = (WebView) findViewById(R.id.webView);
+        webView = (WebView) findViewById(R.id.webView);
 
         webView.clearCache(true);
         webView.clearHistory();
@@ -52,11 +56,17 @@ public class WebViewActivity extends AppCompatActivity {
         public void processHTML(String html) {
             Map<String, String> userDataMap = new HashMap<>();
             Document doc = Jsoup.parse(html);
-            Elements userData = doc.getElementById("resultsTable").select("input[type=hidden]");
+            Element result = doc.getElementById("resultsTable");
+            if(result == null) {
+                Toast.makeText(WebViewActivity.this, "Invalid Input", Toast.LENGTH_LONG).show();
+                return;
+            }
+            Elements userData = result.select("input[type=hidden]");
             for(Element elem : userData) {
                 userDataMap.put(elem.attr("name"), elem.attr("value"));
                 Log.d(elem.attr("name"), elem.attr("value"));
             }
+            UserModel.populateBySignUp(userDataMap);
         }
     }
 }
--- app/src/main/java/com/microsoft/enigma/models/UserModel.java
+++ app/src/main/java/com/microsoft/enigma/models/UserModel.java
@@ -1,6 +1,117 @@
 package com.microsoft.enigma.models;
 
+import java.util.Map;
+
 public class UserModel {
 
+    private static String id;
+    private static String epicNumber;
+    private static String name;
+    private static String gender;
+    private static String age;
+    private static String fatherName;
+    private static String state;
+    private static String district;
+    private static String assemblyConstituencyName;
+    private static String assemblyConstituencyNumber;
+    private static String parliamentaryConstituencyName;
+    private static String pollingStationName;
+    private static String pollingStationLocation;
+    private static String partNumber;
+    private static String partName;
+    private static String serialNumber;
+    private static String lastUpdatedOn;
 
+    // When data is fetched from National Voter's Portal
+    public static void populateBySignUp(Map<String, String> userDataMap) {
+        id = userDataMap.get("id");
+        epicNumber = userDataMap.get("epic_no");
+        name = userDataMap.get("name");
+        gender = userDataMap.get("gender");
+        age = userDataMap.get("age");
+        fatherName = userDataMap.get("rln_name");
+        state = userDataMap.get("state");
+        district = userDataMap.get("district");
+        assemblyConstituencyName = userDataMap.get("ac_name");
+        assemblyConstituencyNumber = userDataMap.get("ac_no");
+        parliamentaryConstituencyName = userDataMap.get("pc_name");
+        pollingStationName = userDataMap.get("ps_name");
+        pollingStationLocation = userDataMap.get("ps_lat_long");
+        partNumber = userDataMap.get("part_no");
+        partName = userDataMap.get("part_name");
+        serialNumber = userDataMap.get("slno_inpart");
+        lastUpdatedOn = userDataMap.get("last_update");
+    }
+
+    // When data is fetched from node.js backend
+    public static void populateByLogin(Map<String, String> userDataMap) {
+    }
+
+    public static String getId() {
+        return id;
+    }
+
+    public static String getEpicNumber() {
+        return epicNumber;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static String getGender() {
+        return gender;
+    }
+
+    public static String getAge() {
+        return age;
+    }
+
+    public static String getFatherName() {
+        return fatherName;
+    }
+
+    public static String getState() {
+        return state;
+    }
+
+    public static String getDistrict() {
+        return district;
+    }
+
+    public static String getAssemblyConstituencyName() {
+        return assemblyConstituencyName;
+    }
+
+    public static String getAssemblyConstituencyNumber() {
+        return assemblyConstituencyNumber;
+    }
+
+    public static String getParliamentaryConstituencyName() {
+        return parliamentaryConstituencyName;
+    }
+
+    public static String getPollingStationName() {
+        return pollingStationName;
+    }
+
+    public static String getPollingStationLocation() {
+        return pollingStationLocation;
+    }
+
+    public static String getPartNumber() {
+        return partNumber;
+    }
+
+    public static String getPartName() {
+        return partName;
+    }
+
+    public static String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public static String getLastUpdatedOn() {
+        return lastUpdatedOn;
+    }
 }
